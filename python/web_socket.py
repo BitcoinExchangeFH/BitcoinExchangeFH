09b6f3e5cde6aeff69ea15fdc15c7db300ce3272
Gavin.Chan <gavincyi@gmail.com>
2016-10-31 00:34:44 +0800

    Support python 2 and 3 compatability

--- python/web_socket.py
+++ python/web_socket.py
@@ -24,11 +24,7 @@ class RESTfulApi:
         :param: url: The url link
         :return JSON object
         """
-        req = urlrequest.Request(url, None, headers={
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Accept": "*/*",
-            "User-Agent": "curl/7.24.0 (x86_64-apple-darwin12.0)"})
-        res = urlrequest.urlopen(req)
+        res = urlrequest.urlopen(url)
         res = json.loads(res.read().decode('utf8'))
         return res
 

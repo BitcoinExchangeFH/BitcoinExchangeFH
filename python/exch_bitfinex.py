2ac41b6b668eab540f5dd71624a90409476eb9ca
Gavin Chan <gavincyi@gmail.com>
2016-12-04 18:17:09 +0800

    Resolved Bitfinex API issue

--- python/exch_bitfinex.py
+++ python/exch_bitfinex.py
@@ -9,24 +9,6 @@ from exchange import ExchangeGateway
 from instrument import Instrument
 from util import print_log
 
-class ExchGwBitfinexInstrument(Instrument):
-    def __init__(self, instmt):
-        """
-        Constructor
-        :param instmt: Instrument
-        """
-        self.copy(instmt)
-        self.subscribed = False
-        self.db_order_book_table_name = ""
-        self.db_trades_table_name = ""
-        self.db_order_book_id = ""
-        self.db_trade_id = ""
-        self.last_exch_trade_id = ""
-        self.book_channel_id = 0
-        self.trades_channel_id = 0
-        self.prev_l2_depth = L2Depth(depth=25)
-        self.l2_depth = L2Depth(depth=25)
-
 
 class ExchGwBitfinexWs(WebSocketApiClient):
     """
@@ -46,22 +28,17 @@ class ExchGwBitfinexWs(WebSocketApiClient):
         :param raw: Raw data in JSON
         """
         # No order book mapping from config. Need to decode here.
-
-        instmt.l2_depth.date_time = datetime.utcnow().strftime("%Y%m%d %H:%M:%S.%f")
+        l2_depth = instmt.get_l2_depth()
+        l2_depth.date_time = datetime.utcnow().strftime("%Y%m%d %H:%M:%S.%f")
         if isinstance(raw[0], list):
             # Start subscription
-            for depth in raw:
-                price = depth[0]
-                count = depth[1]
-                volume = depth[2]
+            for i in range(0, 25):
+                bid = raw[i]
+                ask = raw[25+i]
 
-                if volume > 0:
-                    instmt.l2_depth.bids.append(L2Depth.Depth(price=price, count=count, volume=volume))
-                else:
-                    instmt.l2_depth.asks.append(L2Depth.Depth(price=price, count=count, volume=-volume))
+                l2_depth.bids[i] = L2Depth.Depth(price=bid[0], count=bid[1], volume=bid[2])
+                l2_depth.asks[i] = L2Depth.Depth(price=ask[0], count=ask[1], volume=-ask[2])
 
-            instmt.l2_depth.sort_bids()
-            instmt.l2_depth.sort_asks()
         else:
             price = raw[1]
             count = raw[2]
@@ -71,34 +48,34 @@ class ExchGwBitfinexWs(WebSocketApiClient):
             if count == 0:
                 # Deletion
                 if volume > 0:
-                    for i in range(0, len(instmt.l2_depth.bids)):
-                        if price == instmt.l2_depth.bids[i].price:
+                    for i in range(0, len(l2_depth.bids)):
+                        if price == l2_depth.bids[i].price:
                             found = True
-                            del instmt.l2_depth.bids[i]
+                            del l2_depth.bids[i]
                             break
                 else:
-                    for i in range(0, len(instmt.l2_depth.asks)):
-                        if price == instmt.l2_depth.asks[i].price:
+                    for i in range(0, len(l2_depth.asks)):
+                        if price == l2_depth.asks[i].price:
                             found = True
-                            del instmt.l2_depth.asks[i]
+                            del l2_depth.asks[i]
                             break
 
                 if not found:
                     depth_text = ""
-                    for i in range(0, instmt.l2_depth.depth):
-                        if i < len(instmt.l2_depth.bids):
+                    for i in range(0, l2_depth.depth):
+                        if i < len(l2_depth.bids):
                             depth_text += "%.4f,%d,%.4f" % \
-                              (instmt.l2_depth.bids[i].volume, \
-                               instmt.l2_depth.bids[i].count, \
-                               instmt.l2_depth.bids[i].price)
+                              (l2_depth.bids[i].volume, \
+                               l2_depth.bids[i].count, \
+                               l2_depth.bids[i].price)
                         else:
                             depth_text += "                   "
                         depth_text += "<--->"
-                        if i < len(instmt.l2_depth.asks):
+                        if i < len(l2_depth.asks):
                             depth_text += "%.4f,%d,%.4f" % \
-                                          (instmt.l2_depth.asks[i].volume, \
-                                           instmt.l2_depth.asks[i].count, \
-                                           instmt.l2_depth.asks[i].price)
+                                          (l2_depth.asks[i].volume, \
+                                           l2_depth.asks[i].count, \
+                                           l2_depth.asks[i].price)
                         else:
                             depth_text += "                   "
                         depth_text += "\n"
@@ -108,42 +85,42 @@ class ExchGwBitfinexWs(WebSocketApiClient):
                 # Insertion/Update
                 if volume > 0:
                     # Update
-                    for i in range(0, len(instmt.l2_depth.bids)):
-                        if price == instmt.l2_depth.bids[i].price:
-                            instmt.l2_depth.bids[i].count = count
-                            instmt.l2_depth.bids[i].volume = volume
+                    for i in range(0, len(l2_depth.bids)):
+                        if price == l2_depth.bids[i].price:
+                            l2_depth.bids[i].count = count
+                            l2_depth.bids[i].volume = volume
                             found = True
                             break
 
                     if not found:
                         # Insertion
-                        instmt.l2_depth.bids.append(L2Depth.Depth(price=price,
+                        l2_depth.bids.append(L2Depth.Depth(price=price,
                                                          count=count,
                                                          volume=volume))
-                        instmt.l2_depth.sort_bids()
+                        l2_depth.sort_bids()
 
-                        if len(instmt.l2_depth.bids) > instmt.l2_depth.depth * 2:
-                            del instmt.l2_depth.bids[instmt.l2_depth.depth:]
+                        if len(l2_depth.bids) > l2_depth.depth * 2:
+                            del l2_depth.bids[l2_depth.depth:]
                 else:
-                    for i in range(0, len(instmt.l2_depth.asks)):
+                    for i in range(0, len(l2_depth.asks)):
                         # Update
-                        if price == instmt.l2_depth.asks[i].price:
-                            instmt.l2_depth.asks[i].count = count
-                            instmt.l2_depth.asks[i].volume = -volume
+                        if price == l2_depth.asks[i].price:
+                            l2_depth.asks[i].count = count
+                            l2_depth.asks[i].volume = -volume
                             found = True
                             break
 
                     if not found:
                         # Insertion
-                        instmt.l2_depth.asks.append(L2Depth.Depth(price=price,
+                        l2_depth.asks.append(L2Depth.Depth(price=price,
                                                     count=count,
                                                     volume=-volume))
-                        instmt.l2_depth.sort_asks()
+                        l2_depth.sort_asks()
 
-                        if len(instmt.l2_depth.asks) > instmt.l2_depth.depth * 2:
-                            del instmt.l2_depth.asks[instmt.l2_depth.depth:]
+                        if len(l2_depth.asks) > l2_depth.depth * 2:
+                            del l2_depth.asks[l2_depth.depth:]
 
-        return instmt.l2_depth
+        return l2_depth
 
     @classmethod
     def parse_trade(cls, instmt, raw):
@@ -171,7 +148,7 @@ class ExchGwBitfinexWs(WebSocketApiClient):
                     trade.trade_side = Trade.Side.BUY if value > 0 else Trade.Side.SELL
                     trade.trade_volume = abs(value)
                 elif field == 'TRADE_ID':
-                    trade.trade_id = value
+                    trade.trade_id = str(value)
                 elif field == 'TRADE_PRICE':
                     trade.trade_price = value
 
@@ -205,12 +182,10 @@ class ExchGwBitfinex(ExchangeGateway):
         """
         print_log(self.__class__.__name__, "Instrument %s is subscribed in channel %s" % \
                   (instmt.get_instmt_code(), instmt.get_exchange_name()))
-        if not instmt.subscribed:
-            instmt.l2_depth.bids = []
-            instmt.l2_depth.asks = []
+        if not instmt.get_subscribed():
             ws.send("{\"event\":\"subscribe\", \"channel\": \"book\", \"pair\": \"%s\", \"freq\": \"F0\"}" % instmt.get_instmt_code())
             ws.send("{\"event\":\"subscribe\", \"channel\": \"trades\", \"pair\": \"%s\"}" % instmt.get_instmt_code())
-            instmt.subscribed = True
+            instmt.set_subscribed(True)
 
     def on_close_handler(self, instmt, ws):
         """
@@ -220,7 +195,7 @@ class ExchGwBitfinex(ExchangeGateway):
         """
         print_log(self.__class__.__name__, "Instrument %s is subscribed in channel %s" % \
                   (instmt.get_instmt_code(), instmt.get_exchange_name()))
-        instmt.subscribed = False
+        instmt.set_subscribed(False)
 
     def on_message_handler(self, instmt, message):
         """
@@ -236,50 +211,49 @@ class ExchGwBitfinex(ExchangeGateway):
             elif 'event' in keys and message['event'] == 'subscribed':
                 if instmt.get_instmt_code() == message['pair']:
                     if message['channel'] == 'book':
-                        instmt.book_channel_id = message['chanId']
+                        instmt.set_order_book_channel_id(message['chanId'])
                     elif message['channel'] == 'trades':
-                        instmt.trades_channel_id = message['chanId']
+                        instmt.set_trades_channel_id(message['chanId'])
                     else:
                         raise Exception("Unknown channel %s : <%s>" % (message['channel'], message))
 
                     print_log(self.__class__.__name__, 'Subscription: %s, pair: %s, channel Id: %s' % \
                               (message['channel'], instmt.get_instmt_code(), message['chanId']))
         elif isinstance(message, list):
-            if message[0] == instmt.book_channel_id:
+            if message[0] == instmt.get_order_book_channel_id():
                 if isinstance(message[1], list):
-                    l2_depth = self.api_socket.parse_l2_depth(instmt, message[1])
+                    self.api_socket.parse_l2_depth(instmt, message[1])
                 elif len(message) != 2:
-                    instmt.prev_l2_depth = instmt.l2_depth.copy()
-                    l2_depth = self.api_socket.parse_l2_depth(instmt, message)
+                    instmt.set_prev_l2_depth(instmt.get_l2_depth().copy())
+                    self.api_socket.parse_l2_depth(instmt, message)
                 else:
                     return
 
-                if l2_depth.is_diff(instmt.prev_l2_depth):
-                    instmt.db_order_book_id += 1
-                    self.db_client.insert(table=instmt.db_order_book_table_name,
+                if instmt.get_l2_depth().is_diff(instmt.get_prev_l2_depth()):
+                    instmt.incr_order_book_id()
+                    self.db_client.insert(table=instmt.get_order_book_table_name(),
                                           columns=['id'] + L2Depth.columns(),
-                                          values=[instmt.db_order_book_id] + l2_depth.values())
-
-            elif message[0] == instmt.trades_channel_id:
+                                          values=[instmt.get_order_book_id()] + instmt.get_l2_depth().values())
+            elif message[0] == instmt.get_trades_channel_id():
                 if isinstance(message[1], list):
                     raw_trades = message[1]
                     raw_trades.sort(key=lambda x:x[0])
                     for raw in raw_trades:
                         trade = self.api_socket.parse_trade(instmt, raw)
-                        if trade.trade_id > instmt.last_exch_trade_id:
-                            instmt.db_trade_id += 1
-                            instmt.last_exch_trade_id = trade.trade_id
-                            self.db_client.insert(table=instmt.db_trades_table_name,
+                        if int(trade.trade_id) > int(instmt.get_exch_trade_id()):
+                            instmt.incr_trade_id()
+                            instmt.set_exch_trade_id(trade.trade_id)
+                            self.db_client.insert(table=instmt.get_trades_table_name(),
                                                   columns=['id'] + Trade.columns(),
-                                                  values=[instmt.db_trade_id] + trade.values())
+                                                  values=[instmt.get_trade_id()] + trade.values())
                 elif message[1] == 'tu':
                     trade = self.api_socket.parse_trade(instmt, message[3:])
-                    if trade.trade_id > instmt.last_exch_trade_id:
-                        instmt.db_trade_id += 1
-                        instmt.last_exch_trade_id = trade.trade_id
-                        self.db_client.insert(table=instmt.db_trades_table_name,
+                    if int(trade.trade_id) > int(instmt.get_exch_trade_id()):
+                        instmt.incr_trade_id()
+                        instmt.set_exch_trade_id(trade.trade_id)
+                        self.db_client.insert(table=instmt.get_trades_table_name(),
                                               columns=['id'] + Trade.columns(),
-                                              values=[instmt.db_trade_id] + trade.values())
+                                              values=[instmt.get_trade_id()] + trade.values())
 
     def start(self, instmt):
         """
@@ -287,14 +261,16 @@ class ExchGwBitfinex(ExchangeGateway):
         :param instmt: Instrument
         :return List of threads
         """
-        instmt = ExchGwBitfinexInstrument(instmt)
-        instmt.db_order_book_table_name = self.get_order_book_table_name(instmt.get_exchange_name(),
-                                                                       instmt.get_instmt_name())
-        instmt.db_trades_table_name = self.get_trades_table_name(instmt.get_exchange_name(),
-                                                               instmt.get_instmt_name())
-        instmt.db_order_book_id = self.get_order_book_init(instmt)
-        instmt.db_trade_id, instmt.last_exch_trade_id = self.get_trades_init(instmt)
-        instmt.last_exch_trade_id = int(instmt.last_exch_trade_id)
+        instmt.set_l2_depth(L2Depth(25))
+        instmt.set_prev_l2_depth(L2Depth(25))
+        instmt.set_order_book_table_name(self.get_order_book_table_name(instmt.get_exchange_name(),
+                                                                       instmt.get_instmt_name()))
+        instmt.set_trades_table_name(self.get_trades_table_name(instmt.get_exchange_name(),
+                                                               instmt.get_instmt_name()))
+        instmt.set_order_book_id(self.get_order_book_init(instmt))
+        trade_id, last_exch_trade_id = self.get_trades_init(instmt)
+        instmt.set_trade_id(trade_id)
+        instmt.set_exch_trade_id(last_exch_trade_id)
         return [self.api_socket.connect(instmt.get_link(),
                                         on_message_handler=partial(self.on_message_handler, instmt),
                                         on_open_handler=partial(self.on_open_handler, instmt),

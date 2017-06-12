b996a272d2594cf2174e484ac36f02a4183b2e5d
Administrator <nigelboucher@qq.com>
2017-06-12 08:17:17 +0800

    zmqfeed

--- befh/test/test_zmqfeed.py
+++ befh/test/test_zmqfeed.py
@@ -30,7 +30,7 @@ while True:
                     "JUBI_Spot_SPOT_XRPCNY" in keys and \
                     "JUBI_Spot_SPOT_BTCCNY" in keys:
         ratio = 1 / exchanges_snapshot["Bitfinex_SPOT_XRPBTC"]["a1"] * exchanges_snapshot["JUBI_Spot_SPOT_XRPCNY"][
-            "b1"] / exchanges_snapshot["JUBI_Spot_SPOT_BTCCNY"]["a1"] - 0.005 - 0.001 - 1
+            "b1"] / exchanges_snapshot["JUBI_Spot_SPOT_BTCCNY"]["a1"] - 0.005 - 0.006 - 1
         timekey = "JUBI.CNY_BTC(buy)->Bitfinex.BTC_XRP(buy)->JUBI.XRP_CNY(sell)"
         if timekey not in itchatsendtime.keys():
             itchatsendtime[timekey] = 0
@@ -46,7 +46,7 @@ while True:
             itchatsendtime[timekey] = time.time()
 
         ratio = exchanges_snapshot["Bitfinex_SPOT_XRPBTC"]["b1"] * exchanges_snapshot["JUBI_Spot_SPOT_BTCCNY"]["b1"] / \
-                exchanges_snapshot["JUBI_Spot_SPOT_XRPCNY"]["a1"] - 0.005 - 0.01 - 1
+                exchanges_snapshot["JUBI_Spot_SPOT_XRPCNY"]["a1"] - 0.005 - 0.015 - 1
         timekey = "JUBI.CNY_XRP(buy)->Bitfinex.XRP_BTC(sell)->JUBI.BTC_CNY(sell)"
         if timekey not in itchatsendtime.keys():
             itchatsendtime[timekey] = 0
@@ -65,7 +65,7 @@ while True:
                     "JUBI_Spot_SPOT_ETHCNY" in keys and \
                     "JUBI_Spot_SPOT_BTCCNY" in keys:
         ratio = 1 / exchanges_snapshot["Bitfinex_SPOT_ETHBTC"]["a1"] * exchanges_snapshot["JUBI_Spot_SPOT_ETHCNY"][
-            "b1"] / exchanges_snapshot["JUBI_Spot_SPOT_BTCCNY"]["a1"] - 0.005 - 0.001 - 1
+            "b1"] / exchanges_snapshot["JUBI_Spot_SPOT_BTCCNY"]["a1"] - 0.005 - 0.006 - 1
         timekey = "JUBI.CNY_BTC(buy)->Bitfinex.BTC_ETH(buy)->JUBI.ETH_CNY(sell)"
         if timekey not in itchatsendtime.keys():
             itchatsendtime[timekey] = 0
@@ -81,7 +81,7 @@ while True:
             itchatsendtime[timekey] = time.time()
 
         ratio = exchanges_snapshot["Bitfinex_SPOT_ETHBTC"]["b1"] * exchanges_snapshot["JUBI_Spot_SPOT_BTCCNY"]["b1"] / \
-                exchanges_snapshot["JUBI_Spot_SPOT_ETHCNY"]["a1"] - 0.005 - 0.001 - 1
+                exchanges_snapshot["JUBI_Spot_SPOT_ETHCNY"]["a1"] - 0.005 - 0.006 - 1
         timekey = "JUBI.CNY_ETH(buy)->Bitfinex.ETH_BTC(sell)->JUBI.BTC_CNY(sell)"
         if timekey not in itchatsendtime.keys():
             itchatsendtime[timekey] = 0
@@ -134,7 +134,7 @@ while True:
                     "JUBI_Spot_SPOT_BTCCNY" in keys:
         ratio = 1 / exchanges_snapshot["Bitfinex_SPOT_ETCBTC"]["a1"] * \
                 exchanges_snapshot["JUBI_Spot_SPOT_ETCCNY"][
-                    "b1"] / exchanges_snapshot["JUBI_Spot_SPOT_BTCCNY"]["a1"] - 0.005 - 0.001 - 1
+                    "b1"] / exchanges_snapshot["JUBI_Spot_SPOT_BTCCNY"]["a1"] - 0.005 - 0.006 - 1
         timekey = "JUBI.CNY_BTC(buy)->Bitfinex.BTC_ETC(buy)->JUBI.ETC_CNY(sell)"
         if timekey not in itchatsendtime.keys():
             itchatsendtime[timekey] = 0
@@ -152,7 +152,7 @@ while True:
 
         ratio = exchanges_snapshot["Bitfinex_SPOT_ETCBTC"]["b1"] * exchanges_snapshot["JUBI_Spot_SPOT_BTCCNY"][
             "b1"] / \
-                exchanges_snapshot["JUBI_Spot_SPOT_ETCCNY"]["a1"] - 0.005 - 0.001 - 1
+                exchanges_snapshot["JUBI_Spot_SPOT_ETCCNY"]["a1"] - 0.005 - 0.006 - 1
         timekey = "JUBI.CNY_ETC(buy)->Bitfinex.ETC_BTC(sell)->JUBI.BTC_CNY(sell)"
         if timekey not in itchatsendtime.keys():
             itchatsendtime[timekey] = 0
@@ -161,6 +161,47 @@ while True:
                 "warning: " + "ETC:" + str(exchanges_snapshot["JUBI_Spot_SPOT_ETCCNY"]["a1"]) + " ETCBTC:" + str(
                     exchanges_snapshot["Bitfinex_SPOT_ETCBTC"]["b1"]) + " BTC:" + str(
                     exchanges_snapshot["JUBI_Spot_SPOT_BTCCNY"]["b1"]) + " " + timekey + ": " + "{:.2%}".format(
+                    ratio),
+                toUserName="filehelper")
+            itchatsendtime[timekey] = time.time()
+        elif time.time() - itchatsendtime[timekey] > 600:
+            itchat.send(timekey + ": " + "{:.2%}".format(ratio), toUserName="filehelper")
+            itchatsendtime[timekey] = time.time()
+
+    if "Poloniex_BTCBTS" in keys and \
+                    "JUBI_Spot_SPOT_BTSCNY" in keys and \
+                    "BTCC_Spot_SPOT_BTCCNY" in keys:
+        ratio = 1 / exchanges_snapshot["Poloniex_BTCBTS"]["a1"] * \
+                exchanges_snapshot["JUBI_Spot_SPOT_BTSCNY"][
+                    "b1"] / exchanges_snapshot["BTCC_Spot_SPOT_BTCCNY"]["a1"] - 0.0055 - 0.006 - 1
+        timekey = "BTCC.CNY_BTC(buy)->Poloniex.BTC_BTS(buy)->JUBI.BTS_CNY(sell)"
+        if timekey not in itchatsendtime.keys():
+            itchatsendtime[timekey] = 0
+        if time.time() - itchatsendtime[timekey] > 60 and ratio > 0.01:
+            itchat.send(
+                "warning: " + "BTC:" + str(
+                    exchanges_snapshot["BTCC_Spot_SPOT_BTCCNY"]["a1"]) + " BTSBTC:" + str(
+                    exchanges_snapshot["Poloniex_BTCBTS"]["a1"]) + " BTS:" + str(
+                    exchanges_snapshot["JUBI_Spot_SPOT_BTSCNY"]["b1"]) + " " + timekey + ": " + "{:.2%}".format(
+                    ratio),
+                toUserName="filehelper")
+            itchatsendtime[timekey] = time.time()
+        elif time.time() - itchatsendtime[timekey] > 600:
+            itchat.send(timekey + ": " + "{:.2%}".format(ratio), toUserName="filehelper")
+            itchatsendtime[timekey] = time.time()
+
+        ratio = exchanges_snapshot["Poloniex_BTCBTS"]["b1"] * exchanges_snapshot["BTCC_Spot_SPOT_BTCCNY"][
+            "b1"] / \
+                exchanges_snapshot["JUBI_Spot_SPOT_BTSCNY"]["a1"] - 0.005 - 0.006 - 1
+        timekey = "JUBI.CNY_BTS(buy)->Poloniex.BTS_BTC(sell)->BTCC.BTC_CNY(sell)"
+        if timekey not in itchatsendtime.keys():
+            itchatsendtime[timekey] = 0
+        if time.time() - itchatsendtime[timekey] > 60 and ratio > 0.01:
+            itchat.send(
+                "warning: " + "BTS:" + str(
+                    exchanges_snapshot["JUBI_Spot_SPOT_BTSCNY"]["a1"]) + " BTSBTC:" + str(
+                    exchanges_snapshot["Poloniex_BTCBTS"]["b1"]) + " BTC:" + str(
+                    exchanges_snapshot["BTCC_Spot_SPOT_BTCCNY"]["b1"]) + " " + timekey + ": " + "{:.2%}".format(
                     ratio),
                 toUserName="filehelper")
             itchatsendtime[timekey] = time.time()

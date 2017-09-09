f72dd64ec645ae73fc61cd03b8002f71f93080de
Gavin Chan <gavincyi@gmail.com>
2017-09-09 01:52:57 +0000

    Release version 0.2.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='BitcoinExchangeFH',
-    version='0.2.0',
+    version='0.2.1',
     author='Gavin Chan',
     author_email='gavincyi@gmail.com',
     packages=['befh'],
